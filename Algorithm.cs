6540b2de6f8bfc257c8d5c4077ca28589e6a58ad
Saša Savić <sasa.savic@pm.me>
2019-01-18 22:51:33 +0100

    Add charge factor to Coulombs law implementation

--- Algorithm.cs
+++ Algorithm.cs
@@ -10,13 +10,13 @@ namespace Buzz
     {
         private static readonly float CoulombsConst = 1000.0f;
         public static Vector2 CoulombsLaw(Particle p1, Particle p2) =>
-            CoulombsLaw(p1.Position - p2.Position);
+            CoulombsLaw(p1.Position - p2.Position, (p1.Charge == p2.Charge) ? -1 : 1);
 
-        public static Vector2 CoulombsLaw(Vector2 r) 
+        public static Vector2 CoulombsLaw(Vector2 r, int direction_factor = 1) 
         {
             if (r.Length() == 0) 
                 return Vector2.Zero;
-            return CoulombsConst / r.LengthSquared() * r;
+            return CoulombsConst / r.LengthSquared() * r * direction_factor;
         }
     }
 }
